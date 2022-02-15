43bd0fcf2639b79e435c7ea8772f7db63e7c6bc0
Sy Rashid <sy.rashid.11@gmail.com>
2022-02-15 11:43:31 +0000

    FEAT: Finish livecode

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # CRUD = Create Read Update Delete/Destroy
+  # Verb "URI", to: "controller_prefex#action"
+
+  # Read all the restaurants
+  # get 'restaurants', to: 'restaurants#index'
+  # # Create restaurants
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+  # # Read a single restaurant
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+  # # Update restaurants
+  # get 'restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  # patch 'restaurants/:id', to: 'restaurants#update'
+  # # Destroy a restaurant
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+  resources :restaurants
 end
